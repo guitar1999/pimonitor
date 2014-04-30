f9bafb11cd15ce130151718cd24a299869c32c10
Jesse Bishop <jessebishop@blackosprey.com>
2014-04-30 21:03:59 +0000

    first round of monitoring

--- check_remote_status.py
+++ check_remote_status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import argparse, ConfigParser, psycopg2
+from tweet import *
 
 config = ConfigParser.RawConfigParser()
 config.read('/home/jessebishop/.pyconfig')
@@ -15,10 +16,29 @@ args = p.parse_args()
 db = psycopg2.connect(host=dbhost, database=dbname, user=dbuser)
 cursor = db.cursor()
 
-query = """SELECT (CURRENT_TIMESTAMP - timestamp) > interval '5 minutes' AS success, notified FROM pi_checkin WHERE pi = {0};""".format(args.piname)
+query = """SELECT (CURRENT_TIMESTAMP - timestamp) < interval '5 minutes' AS success, notified FROM pi_checkin WHERE pi = '{0}';""".format(args.piname)
 try:
     cursor.execute(query)
-    cursor.commit()
+    success, notified = cursor.fetchall()[0]
+    if not success:
+        if not notified:
+            #send a tweet
+            status = """Hey @jessebishop, {0} hasn't been heard from in over 5 minutes!""".format(args.piname)
+            tweet(status)
+            query = """UPDATE pi_checkin SET notified = TRUE WHERE pi = '{0}';""".format(args.piname)
+            cursor.execute(query)
+        else:
+            query = """SELECT (CURRENT_TIMESTAMP - timestamp) > interval '1 day' AS check FROM pi_checkin WHERE pi = '{0}';""".format(args.piname)
+            cursor.execute(query)
+            check = cursor.fetchall()[0][0]
+            if check:
+                status = """Hey @jessebishop, {0} hasn't been heard from in over a day!""".format(args.piname)
+                tweet(status)
+                query = """UPDATE pi_checkin SET timestamp = CURRENT_TIMESTAMP - interval '6 minutes' WHERE pi = '{0}';""".format(args.piname)
+                cursor.execute(query)
+    else:
+        query = """UPDATE pi_checkin SET notified = FALSE WHERE pi = '{0}'""".format(args.piname)
+        cursor.execute(query)
 except Exception, msg:
     print msg
 cursor.close()
